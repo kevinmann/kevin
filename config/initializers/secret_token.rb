9595c49d4393924cfe35cb5b086978580516e309
vagrant <vagrant@audacious.(none)>
2012-12-11 16:31:40 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kevin::Application.config.secret_token = 'd0a6685c530c9e1b961c2895e402fdd362646df37fa638bb872a78e82c3561f72ba55a68149d92e27c419120cc6f03bce78b7871774a85522c30ebcedab0df9a'
